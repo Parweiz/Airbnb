1ed4b5f3e1411f2190fa9d42a3dea7218c346ac0
Parweiz <parweizhaqshenas@hotmail.com>
2021-09-23 23:45:59 +0200

    Implemented calender and search functionaltiy

--- pages/index.js
+++ pages/index.js
@@ -24,12 +24,12 @@ export default function Home(props) {
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {exploreData?.map((item) => (
+            {exploreData?.map(({img, location, distance}) => (
               <SmallCard
-                key={item.img}
-                img={item.img}
-                location={item.location}
-                distance={item.distance}
+                key={img}
+                img={img}
+                location={location}
+                distance={distance}
               />
             ))}
           </div>
@@ -39,14 +39,19 @@ export default function Home(props) {
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
 
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
-            {cardsData?.map((item) => (
-              <MediumCard key={item.img} img={item.img} title={item.title} />
+            {cardsData?.map(({img, title}) => (
+              <MediumCard key={img} img={img} title={title} />
             ))}
           </div>
         </section>
 
         <section>
-          <LargeCard img="https://links.papareact.com/4cj" title="The Greatest Outdoors" description="Wishlists curated by Airbnb." buttonText="Get Inspired"/>
+          <LargeCard
+            img="https://links.papareact.com/4cj"
+            title="The Greatest Outdoors"
+            description="Wishlists curated by Airbnb."
+            buttonText="Get Inspired"
+          />
         </section>
       </main>
 
